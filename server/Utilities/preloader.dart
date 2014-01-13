83cac80b965d8598b5ff408c67e0442ee317f0e7
Thomas Caserta <tomcaserta@gmail.com>
2014-01-13 09:46:49 +0000

    Updated preloader to allow for non future based

--- server/Utilities/preloader.dart
+++ server/Utilities/preloader.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 class PreloadElement { 
   String name;
   Function f;
+  bool isFuture = true;
   PreloadElement(this.name, this.f);
 }
 
@@ -13,31 +14,53 @@ class Preloader {
   StreamController<int> _percent = new StreamController<int>();
   int _waiting = 0;
   int _loaded = 0;
+  int _prevVal = 0;
+  bool loaded = false;
   Preloader();
   void addFuture (PreloadElement f) {
     futures.add(f);
     _waiting++;
   }
-  
+  void addMethod (PreloadElement f) {
+    f.isFuture = false;
+    futures.add(f);
+    _waiting++;
+  }
+  void _inc (bool value, PreloadElement ple, Function onError(String err)) {
+    if (value == true) {
+      _loaded++;
+      int perc = _loaded ~/ (_waiting / 100);
+      if (perc != _prevVal) { 
+        _percent.add(perc);
+        _prevVal = perc;
+      }
+      if (_loaded == _waiting) {
+        loaded = true;
+        _percent.close();
+      }
+    }
+    else {
+      onError("${ple.name} did not complete successfully.");
+    }
+  }
   Stream<int> startLoad ({Function onError(String s) }) {
-    if (onError == null) onError = (e) { };
-    futures.forEach((PreloadElement ple) {
+    if (onError == null) onError = (e) { throw e; };
+    for (PreloadElement ple in futures) {
+      if (ple.isFuture) {
       ple.f().then((bool value) {
-        if (value == true) {
-          _loaded++;
-          _percent.add(_loaded ~/ (_waiting / 100));
-          if (_loaded == _waiting) {
-            _percent.close();
-          }
-        }
-        else {
-          onError("${ple.name} did not complete successfully.");
-        }
+        _inc(value, ple, onError);
       }).catchError((e) { 
         onError(e);
       }); 
-    });
-    
+      }
+      else {  
+        ///TODO: FIX SO IT DOESNT USE A TIMER
+        new Timer(new Duration(milliseconds: 1), () {  
+            bool x = ple.f();
+          _inc(x, ple, onError); 
+        });
+      }
+    }
     return _percent.stream;
   }
 }
