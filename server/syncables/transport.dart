88df69c4328bca53cc78ea5d9a7c507db0f99b76
Thomas Caserta <tomcaserta@gmail.com>
2014-02-18 16:52:47 +0000

    Added terms and terms ref to transport

--- server/syncables/transport.dart
+++ server/syncables/transport.dart
@@ -25,53 +25,73 @@ class Transport extends SyncCachable<Transport> {
   String _quickbooksName = "";
   String _transportSheetEmail = "";
   String _remittanceEmail = "";
-  
-  Map<String, dynamic> toJson () { 
-          return super.toJson()..addAll({ "name": name,
-                                          "quickbooksName": quickbooksName,
-                                          "transportSheetEmail": transportSheetEmail,
-                                          "remittanceEmail": remittanceEmail 
-                                          });
-  }
-  
+  String _termsRef;  
+  int _terms;
 
   String get name => _name;
   String get quickbooksName => _quickbooksName;
   String get transportSheetEmail => _transportSheetEmail;
   String get remittanceEmail => _remittanceEmail;
+  String get termsRef => _termsRef;
+  int get terms => _terms;
   
-  set name ( String name) {
+  Map<String, dynamic> toJson () { 
+          return super.toJson()..addAll({ "name": name,
+                                          "quickbooksName": quickbooksName,
+                                          "transportSheetEmail": transportSheetEmail,
+                                          "remittanceEmail": remittanceEmail,
+                                          "termsRef": termsRef,
+                                          "terms": terms
+                                          });
+  }
+  
+  
+  set name (String name) {
     if (name != _name) {
       _name = name;
       requiresDatabaseSync();
     }
   }
-  set quickbooksName (  String quickbooksName) {
+  set quickbooksName (String quickbooksName) {
     if (quickbooksName != _quickbooksName) {
       _quickbooksName = quickbooksName;
       requiresDatabaseSync();
     }
   }
-  set transportSheetEmail (  String transportSheetEmail) {
+  set transportSheetEmail (String transportSheetEmail) {
     if (transportSheetEmail != _transportSheetEmail) {
       _transportSheetEmail = transportSheetEmail;
       requiresDatabaseSync();
     }
   }
-  set remittanceEmail (  String remittanceEmail) {
+  set remittanceEmail (String remittanceEmail) {
     if (remittanceEmail != _remittanceEmail) {
       _remittanceEmail = remittanceEmail;
       requiresDatabaseSync();
     }
   }
+
+  set termsRef (  String termsRef) {
+    if (termsRef != _termsRef) {
+      _termsRef = termsRef;
+      requiresDatabaseSync();
+    }
+  }
   
-  Transport._create (int ID, String name, this._quickbooksName, this._surcharges, this._transportSheetEmail, this._remittanceEmail):super(ID, name) {
+  set terms (int terms) {
+    if (terms != _terms) {
+      _terms = terms;
+      requiresDatabaseSync();
+    }
+  }
+  
+  Transport._create (int ID, String name, this._quickbooksName, this._surcharges, this._transportSheetEmail, this._remittanceEmail, this._termsRef, this._terms):super(ID, name) {
     this._name = name;
   }
 
-  factory Transport (int ID, String name, String quickbooksName, List<Surcharge> surcharges, String transportSheetEmail, String remittanceEmail) {
+  factory Transport (int ID, String name, String quickbooksName, List<Surcharge> surcharges, String transportSheetEmail, String remittanceEmail, String termsRef, int terms) {
     if (!exists(name)) {
-      return new Transport._create(ID, name, quickbooksName, surcharges, transportSheetEmail, remittanceEmail);
+      return new Transport._create(ID, name, quickbooksName, surcharges, transportSheetEmail, remittanceEmail, termsRef, terms);
     }
     else return get(name);
   }
@@ -81,7 +101,7 @@ class Transport extends SyncCachable<Transport> {
     Completer c = new Completer();
     String surchargesString = _surcharges.join(",");
     if (this.isNew) {
-      dbh.prepareExecute("INSERT INTO transport (name, remittanceEmail, transportSheetEmail, surcharges, quickbooksName) VALUES (?,?,?,?,?)", [name, remittanceEmail,  transportSheetEmail, surchargesString.toString(), quickbooksName])
+      dbh.prepareExecute("INSERT INTO transport (name, remittanceEmail, transportSheetEmail, surcharges, quickbooksName, termsRef, terms) VALUES (?,?,?,?,?,?,?)", [name, remittanceEmail,  transportSheetEmail, surchargesString.toString(), quickbooksName, termsRef, terms])
          .then((Results res) {
             if (res.insertId != 0) {
                this._firstInsert(res.insertId);
@@ -94,7 +114,7 @@ class Transport extends SyncCachable<Transport> {
          .catchError((e) => c.completeError(e));
     }
     else {
-      dbh.prepareExecute("UPDATE transport SET name=?, remittanceEmail=?, transportSheetEmail=?, surcharges=?, quickbooksName=? WHERE ID=?", [name, remittanceEmail, transportSheetEmail, surchargesString.toString(), quickbooksName, ID])
+      dbh.prepareExecute("UPDATE transport SET name=?, remittanceEmail=?, transportSheetEmail=?, surcharges=?, quickbooksName=?, termsRef=?, terms=? WHERE ID=?", [name, remittanceEmail, transportSheetEmail, surchargesString.toString(), quickbooksName, ID, termsRef, terms])
          .then((Results res) {
             if (res.affectedRows <= 1) {
               this.synced();
@@ -115,7 +135,7 @@ class Transport extends SyncCachable<Transport> {
   static Future<bool> init () {
     Completer c = new Completer();
     ffpServerLog.info("Loading transport list...");
-    dbHandler.query("SELECT ID, name, remittanceEmail, transportSheetEmail, surcharges, quickbooksName FROM transport").then((Results res) {
+    dbHandler.query("SELECT ID, name, remittanceEmail, transportSheetEmail, surcharges, quickbooksName, termsRef, terms FROM transport").then((Results res) {
        res.listen((Row data) {
          int ID = data[0];
          String name = data[1];
@@ -123,14 +143,15 @@ class Transport extends SyncCachable<Transport> {
          String transportSheetEmail = data[3];
          String surchargeL = data[4].toString();
          String quickbooksName = data[5];
-
+         String termsRef = data[6];
+         int terms = data[7];
          List<String> surchargeList = surchargeL.split(",");
          List<Surcharge> surcharges = new List<Surcharge>();
          surchargeList.forEach((String sur) {
            List<String> splitSur = sur.split(":");
            surcharges.add(new Surcharge.parse(splitSur[0], splitSur[1]));
          });
-         new Transport(ID, name, quickbooksName, surcharges,  transportSheetEmail, remittanceEmail);
+         new Transport(ID, name, quickbooksName, surcharges,  transportSheetEmail, remittanceEmail, termsRef, terms);
        },
        onDone: () {
          ffpServerLog.info("Loaded transport list");
